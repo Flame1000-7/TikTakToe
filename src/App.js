ee240e553ac3e4ae4c8d29e16a5748aac4ca5e12
Flame1000-7 <kolipzpzol123@gmail.com>
2023-03-04 21:51:28 +0300

    Создание

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const mass = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    const [pole, setPole] = useState(["", "", "", "", "", "", "", "", ""]);
+    const [symbol, setSymbol] = useState("X");
+    return (
+        <div className="TikTakToe">
+            {pole.map((item, index) => {
+                return (
+                    <div
+                        className="play"
+                        onClick={() => {
+                            const newPole = pole.slice(0);
+                            newPole[index] = symbol;
+                            if (symbol == "X") {
+                                setSymbol("O");
+                            }
+                            if (symbol != "X") {
+                                setSymbol("X");
+                            }
+                            setTimeout(() => {
+                                let a = true;
+                                for (let i = 0; i < mass.length; i++) {
+                                    if (
+                                        newPole[mass[i][0]] == "X" &&
+                                        newPole[mass[i][1]] == "X" &&
+                                        newPole[mass[i][2]] == "X"
+                                    ) {
+                                        alert("ВЫИГРАЛИ КРЕСТИКИ");
+                                        a = false;
+                                    }
+                                    if (
+                                        newPole[mass[i][0]] == "O" &&
+                                        newPole[mass[i][1]] == "O" &&
+                                        newPole[mass[i][2]] == "O"
+                                    ) {
+                                        alert("ВЫИГРАЛИ НОЛИКИ");
+                                        a = false;
+                                    }
+                                    if (
+                                        
+                                    ) {
+                                        alert("НИЧЬЯ");
+                                        a = false;
+                                    }
+                                }
+                                if (a == false) {
+                                    setPole([
+                                        "",
+                                        "",
+                                        "",
+                                        "",
+                                        "",
+                                        "",
+                                        "",
+                                        "",
+                                        "",
+                                    ]);
+                                }
+                            }, 0);
+                            setPole(newPole);
+                        }}
+                    >
+                        <p>{pole[index]}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
